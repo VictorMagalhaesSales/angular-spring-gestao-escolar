faa1295d945a4eba9e29ed5206aa04731e79b0c7
Victor Magalhaes Sales <victor.sales@MVFOR.local>
2018-09-11 16:52:14 -0300

    BackEnd

--- ProjetoEscola_back/src/main/java/com/escola/api/exceptionhandler/EscolaExceptionHandler.java
+++ ProjetoEscola_back/src/main/java/com/escola/api/exceptionhandler/EscolaExceptionHandler.java
@@ -4,9 +4,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
 import javax.validation.ConstraintViolationException;
 
-import org.apache.tomcat.util.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -47,7 +48,7 @@ public class EscolaExceptionHandler extends ResponseEntityExceptionHandler{
 	@ExceptionHandler({ DataIntegrityViolationException.class } )
 	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
 		String mensagemParaOUsuario = "Entidade requerida não encontrada";
-		String mensagemParaODesenvolvedor = ex.toString();
+		String mensagemParaODesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
 		List<Erro> erros = Arrays.asList(new Erro(mensagemParaOUsuario, mensagemParaODesenvolvedor));
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
@@ -55,7 +56,7 @@ public class EscolaExceptionHandler extends ResponseEntityExceptionHandler{
 	@ExceptionHandler({EmptyResultDataAccessException.class})
 	public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request){
 		String mensagemParaOUsuario = "Entidade requerida não encontrada";
-		String mensagemParaODesenvolvedor = ex.toString();
+		String mensagemParaODesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
 		List<Erro> erros = Arrays.asList(new Erro(mensagemParaOUsuario, mensagemParaODesenvolvedor));
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
